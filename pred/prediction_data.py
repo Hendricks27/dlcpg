4c5d000f2fbf89248b16a8e9b6df4814bb503f3e
Hendricks27 <wenjinzhang95@gmail.com>
2022-02-18 22:13:24 -0600

    tweaks

--- pred/prediction_data.py
+++ pred/prediction_data.py
@@ -17,11 +17,19 @@ model_path = []
 
 data_name = "f96_fraction"
 calibration_file_path = "./dataset/f96_fraction.csv"
-model_path = []
+model_path = [
+    "full_hc_d0196_fraction_1fd8363d9af9dcad3be7553c39396960.hdf5",
+    "full_hc_d0196_fraction_8c22d89667368f2f02ad996469ba157e.hdf5",
+    "full_hc_d0196_fraction_cb975cfdd4105f97efa0b3afffe075cc.hdf5",
+]
 
 data_name = "f96_retention"
 calibration_file_path = "./dataset/f96_retention.csv"
-model_path = []
+model_path = [
+    "full_hc_d0196_retention_1fd8363d9af9dcad3be7553c39396960.hdf5",
+    "full_hc_d0196_retention_8c22d89667368f2f02ad996469ba157e.hdf5",
+    "full_hc_d0196_retention_cb975cfdd4105f97efa0b3afffe075cc.hdf5",
+]
 
 
 
